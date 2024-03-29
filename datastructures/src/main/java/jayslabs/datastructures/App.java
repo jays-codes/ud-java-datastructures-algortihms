c3260d018b20e78b2db94afbe6ad2a85e04e1c0f
jay menorca <zaimenorca@gmail.com>
2024-03-29 19:32:13 -0400

    0; created datastructures proj; initial repo commit

---
+++ datastructures/src/main/java/jayslabs/datastructures/App.java
@@ -0,0 +1,13 @@
+package jayslabs.datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Wazup. J21 in da haus." );
+    }
+}
---
+++ datastructures/src/main/java/jayslabs/datastructures/HWorld.java
@@ -0,0 +1,7 @@
+package jayslabs.datastructures;
+
+public class HWorld {
+void main() {
+	System.out.println("Wazup. J21 in da haus.");
+}
+}
