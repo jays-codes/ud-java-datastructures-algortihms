ee790c900a8c4f91f3cd32f3d1737c3eb3bcd1da
jay menorca <zaimenorca@gmail.com>
2024-04-03 08:38:23 -0400

    DLL; prepend(), removeFirst(), get(int):Node, set(int, int):boolean

--- datastructures/src/main/java/jayslabs/datastructures/dll/DoublyLinkedList.java
+++ datastructures/src/main/java/jayslabs/datastructures/dll/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package jayslabs.datastructures.dll;
 
+import jayslabs.datastructures.dll.Node;
+
 public class DoublyLinkedList {
 	private Node head;
 	private Node tail;
@@ -60,6 +62,80 @@ public class DoublyLinkedList {
 		} while (ptr!=null);
 		System.out.print("\n");
 
+	}
+
+	public void prepend(int value) {
+		System.out.println("prepend()");
+		Node node = new Node(value);
+		
+		if (length==0) {
+			this.head=node;
+			this.tail=node;
+		}
+		if (length>0) {
+			head.prev=node;
+			node.next=head;
+			head=node;
+		}
+		length++;
+	}
+	
+	public Node removeFirst() {
+		System.out.println("removeFirst()");
+		if (length==0) return null;
+
+		Node temp=head;
+
+		if (head.next==null) {
+			tail=null;
+			head=null;
+		} else {
+			head = head.next;
+			head.prev=null;
+			temp.next=null;		
+		}
+		
+		length--;
+		return temp;
+	}
+	
+	public Node get(int idx) {
+		System.out.println("get()");
+
+		if (idx<0 || idx>=length) {
+			System.out.println("invalid index specified.");
+			return null;
+		}
+		
+		int mid=length/2;
+		
+		Node ptr=null;
+		if (idx<mid) {
+			System.out.println("from head...");
+			ptr=head;
+			for (int i=0;i<idx;i++) {
+				ptr=ptr.next;
+			}
+		} else {
+			System.out.println("from tail...");
+			ptr=tail;
+			for (int i=length-1;i>idx;i--) {
+				ptr=ptr.prev;
+			}			
+		}
+		
+		return ptr;
+	}
+
+	public boolean set(int ix, int val) {
+		System.out.println("set()");
+
+		if (length==0 || ix>=length) return false;
+		
+		Node update = get(ix);
+		if (update==null) return false;
+		update.value=val;
+		return true;
 	}
 	
 }
--- datastructures/src/test/java/jayslabs/datastructures/DoublyLinkedListTest.java
+++ datastructures/src/test/java/jayslabs/datastructures/DoublyLinkedListTest.java
@@ -50,5 +50,101 @@ public class DoublyLinkedListTest {
 //		dll.printNodes();
 
 	}
+	
+    /**
+     * Prepend test
+     */
+	@Test
+	public void testDLLprepend() {
+		DoublyLinkedList dll = new DoublyLinkedList(1);
+		dll.append(2);
+		dll.append(3);
+		dll.append(4);
+		dll.append(5);
+		dll.append(6);
+		dll.append(7);
+		dll.printNodes();
+		dll.prepend(8);
+		dll.prepend(9);
+		dll.prepend(10);		
+		dll.printNodes();
+		assertTrue(true);
+	}
+	
+    /**
+     * RemoveFirst() test
+     */
+	@Test
+	public void testDLLremoveFirst() {
+		DoublyLinkedList dll = new DoublyLinkedList(1);
+		dll.append(2);
+		dll.append(3);
+		dll.append(4);
+		dll.append(5);
+		dll.append(6);
+		dll.append(7);
+		dll.printNodes();
+		dll.prepend(8);
+		dll.prepend(9);
+		dll.prepend(10);		
+		dll.printNodes();
+		Node removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		dll.printNodes();
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();		
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		removed=dll.removeFirst();
+		dll.printNodes();
 
+		assertTrue(true);
+	}
+	
+    /**
+     * Get() test
+     */
+	@Test
+	public void testDLLget() {
+		DoublyLinkedList dll = new DoublyLinkedList(1);
+		dll.append(2);
+		dll.append(3);
+		dll.append(4);
+		dll.append(5);
+		dll.append(6);
+		dll.append(7);
+		dll.printNodes();
+		dll.prepend(8);
+		dll.prepend(9);
+		dll.prepend(10);		
+		dll.printNodes();
+		Node node = dll.get(7);
+		System.out.println("val=" + node.getValue());
+		assertTrue(node.getValue()==5);
+	}
+
+    /**
+     * Set() test
+     */
+	@Test
+	public void testDLLSet() {
+		DoublyLinkedList dll = new DoublyLinkedList(1);
+		dll.append(2);
+		dll.append(3);
+		dll.append(4);
+		dll.append(5);
+		dll.append(6);
+		dll.append(7);
+		dll.printNodes();
+		dll.prepend(8);
+		dll.prepend(9);
+		dll.prepend(10);		
+		dll.printNodes();
+		assertTrue(dll.set(9,11));
+		dll.printNodes();
+
+	}
 }
