2c6052436387c120a74eff044909b3437a9c84f7
jay menorca <zaimenorca@gmail.com>
2024-04-10 13:34:45 -0400

    Hashtable: hash(), set(k,v), get(key):val, keys():ArrayList

--- datastructures/src/main/java/jayslabs/datastructures/hashtable/HashTable.java
+++ datastructures/src/main/java/jayslabs/datastructures/hashtable/HashTable.java
@@ -1,10 +1,17 @@
 package jayslabs.datastructures.hashtable;
 
+import java.util.ArrayList;
+
 public class HashTable {
 	private int size;
 	private Node[] datamap;
 	
 	public HashTable() {
+		datamap = new Node[size];
+	}
+
+	public HashTable(int size) {
+		this.size=size;
 		datamap = new Node[size];
 	}
 
@@ -17,5 +24,67 @@ public class HashTable {
                 temp = temp.next;
             }
         }
+    }
+    
+    public int hash(String key) {
+    	int hashed=0;
+    	
+    	char[] keyarr = key.toCharArray();
+    	int ascii = 0;
+    	
+    	for (int i=0;i<keyarr.length;i++) {
+    		ascii = keyarr[i];
+    		hashed = (hashed + ascii * 31) % datamap.length;
+    	}
+    	
+    	return hashed;
+    }
+    
+    public void set(String key, int value) {
+    	int idx = hash(key);
+    	
+    	Node node = new Node(key,value);
+    	if (datamap[idx]==null) {
+    		datamap[idx]=node;
+    	} else {
+    		Node ptr = datamap[idx];
+    		while (ptr.next!=null)
+    			ptr=ptr.next;
+    		ptr.next=node;
+    	}
+    }
+    
+    public int get(String key) {
+    	int idx = hash(key);
+    	Node node = datamap[idx];
+    	
+    	if (node==null) return 0;
+    	
+    	if (node.next==null && node.key.equals(key))
+    		return node.value;
+    		
+    	while (node!=null) {
+    		if (node.key.equals(key)) {
+        		return node.value;    			
+    		} else {
+    			node=node.next;    			
+    		}
+    	}
+    	return 0;
+    }
+    
+    public ArrayList keys() {
+    	ArrayList<String> allkeys = new ArrayList<>();
+    	
+    	Node item=null;
+    	for (int i=0;i<datamap.length;i++) {
+    		item = datamap[i];
+    		while (item!=null) {
+    			allkeys.add(item.key);
+    			item=item.next;
+    		}
+    	}
+    	
+    	return allkeys;
     }
 }
--- datastructures/src/test/java/jayslabs/datastructures/HashtableTest.java
+++ datastructures/src/test/java/jayslabs/datastructures/HashtableTest.java
@@ -5,11 +5,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import jayslabs.datastructures.hashtable.HashTable;
-import jayslabs.datastructures.tree.BST;
 
 public class HashtableTest {
     /**
-     * test Tree insert() 
+     * test Hashtable printTable() 
      */
 	@Test
 	public void testHashTablePrint() {
@@ -20,4 +19,106 @@ public class HashtableTest {
 
 		assertTrue(true);
 	}
+
+    /**
+     * test Hashtable has() 
+     */
+	@Test
+	public void testHashTableHash() {
+		
+		HashTable ht = new HashTable(6);
+		
+		String key="magic7asdfasd";
+		int hashed = ht.hash(key);
+		System.out.println("hashed: " + hashed);
+		assertTrue(true);
+	}
+
+	/**
+     * test Hashtable set() 
+     */
+	@Test
+	public void testHashTableSet() {
+		
+        HashTable myHashTable = new HashTable(6);
+
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("lumber0", 80);
+        myHashTable.set("lumber1", 81);
+        myHashTable.set("lumber2", 82);
+        myHashTable.set("lumber3", 83);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
+        myHashTable.printTable();
+        
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            0:
+            1:
+            2:
+            3:
+               {screws= 140}
+            4:
+               {bolts= 200}
+            5:
+            6:
+               {nails= 100}
+               {tile= 50}
+               {lumber= 80}
+
+        */
+
+		assertTrue(true);
+	}
+	
+	/**
+     * test Hashtable get() 
+     */
+	@Test
+	public void testHashTableGet() {
+		
+        HashTable ht = new HashTable(6);
+
+        ht.set("nails", 100);
+        ht.set("tile", 50);
+        ht.set("lumber", 80);
+        ht.set("lumber0", 80);
+        ht.set("lumber1", 81);
+        ht.set("lumber2", 82);
+        ht.set("lumber3", 83);
+        ht.set("bolts", 200);
+        ht.set("screws", 140);
+        ht.printTable();
+        
+        System.out.println("lumber2: " + ht.get("lumber2"));
+		assertTrue(true);
+	}
+	
+	/**
+     * test Hashtable keys() 
+     */
+	@Test
+	public void testHashTableKeys() {
+		
+        HashTable ht = new HashTable(6);
+
+        ht.set("nails", 100);
+        ht.set("tile", 50);
+        ht.set("lumber", 80);
+        ht.set("lumber0", 80);
+        ht.set("lumber1", 81);
+        ht.set("lumber2", 82);
+        ht.set("lumber3", 83);
+        ht.set("bolts", 200);
+        ht.set("screws", 140);
+        ht.printTable();
+        
+        System.out.println("keys:" + ht.keys());
+		assertTrue(true);
+	}
+
 }
